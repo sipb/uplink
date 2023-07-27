45f097d07b88a9cddd93616cbf1f7fbdde3eb674
Gabriel Rodríguez <rgabriel@mit.edu>
2023-07-26 21:56:26 -0400

    attempt to disable room previews

--- uplink_synapse_module/src/uplink_synapse_module/first_login.py
+++ uplink_synapse_module/src/uplink_synapse_module/first_login.py
@@ -46,11 +46,25 @@ class UplinkFirstLoginModule:
             }
 
         # Send server notice
-        # Note: relies on implementation details
+        # Note: It relies on implementation details
         # The "correct" way would be to use the admin REST API
-        await self.api._hs.get_server_notices_manager().send_notice(
+        event = await self.api._hs.get_server_notices_manager().send_notice(
             user_id=user,
             event_content=event_content,
         )
+
+
+        # Disable URL previews
+        # Note: It relies on implementation details and an undocumented
+        # (https://github.com/matrix-org/matrix-spec/issues/394) account data
+        # TODO: this does not work for some odd reason! however calling this directly works
+        res = await self.api._hs.get_account_data_handler().add_account_data_to_room(
+            user_id=user,
+            room_id=event.room_id,
+            account_data_type='org.matrix.room.preview_urls',
+            content={'disable': True},
+        )
+
+        print("done adding account data?", res)
 
 
